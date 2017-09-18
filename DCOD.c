a55fdfa1fb46af63bf8b4682c26ffa205c75e374
Howard Stewart <howarq@yahoo.com>
2017-09-18 13:01:24 +0000

    Correct an error in naming (former key.press.c)

--- DCOD.c
+++ DCOD.c
@@ -1,254 +1,163 @@
-#! /usr/bin/tcc -run
-
-/*** includes ***/
+#! /usr/bin/tcc -run 
 
 #include <ctype.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
-/*** defines ***/
+enum KEY_ACTION{
+        KEY_NULL = 0,       /* NULL */
+        CTRL_C = 3,         /* Ctrl-c */
+        CTRL_D = 4,         /* Ctrl-d */
+        CTRL_F = 6,         /* Ctrl-f */
+        CTRL_H = 8,         /* Ctrl-h */
+        TAB = 9,            /* Tab */
+        CTRL_L = 12,        /* Ctrl+l */
+        ENTER = 13,         /* Enter */
+        CTRL_Q = 17,        /* Ctrl-q */
+        CTRL_S = 19,        /* Ctrl-s */
+        CTRL_U = 21,        /* Ctrl-u */
+        ESC = 27,           /* Escape */
+        BACKSPACE =  127,   /* Backspace */
+        /* The following are just soft codes, not really reported by the
+         * terminal directly. */
+        ARROW_LEFT = 1000,
+        ARROW_RIGHT,
+        ARROW_UP,
+        ARROW_DOWN,
+        DEL_KEY,
+        HOME_KEY,
+        END_KEY,
+        PAGE_UP,
 
-#define KILO_VERSION "0.0.1"
-
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-enum editorKey {
-  ARROW_LEFT = 'a',
-  ARROW_RIGHT = 'd',
-  ARROW_UP = 'w',
-  ARROW_DOWN = 's'
+        PAGE_DOWN
 };
 
-/*** data ***/
+struct parse
+{
+       int seqno;         // seqno > 0 implies delay processing
+       int pr;            // process retchar
+       int retchar;      // return character 
+};          
 
-struct editorConfig {
-  int cx, cy;
-  int screenrows;
-  int screencols;
-  struct termios orig_termios;
-};
+struct parse decode = { .seqno = 0, .pr = 1 }; //decode is global 
 
-struct editorConfig E;
 
-/*** terminal ***/
+//  a- c != 27 && seqno == 0   retval = c;
+//  b- c == 27 && seqno == 0   seqno = 1 ; retval = ?;
+//  d- c != 91 && seqno == 1   seqno = 0 ; retval = possible; 
+//  e- c == 91 && seqno == 1   seqno = 2 ; retval = ?;
+// int testa = (c==72)||(c==70)||(c==65);
+//  f- testa   && seqno == 2   seqno = 0 ; retval = arrows;
+// int testb = (c==50)||(c==51)||)c==53)||(c==54);
+//  g- testb   && seqno == 2   seqno = 3 ; retval = ?;
 
-void die(const char *s) {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+int uncode(char c) {
 
-  perror(s);
-  exit(1);
+  static char seq[4];
+
+  if ((c != 27) && (decode.seqno == 0 )) 
+     {//printf ("nop %d \n",c);
+      decode.pr=1; 
+      decode.retchar = (int) c ; return 0;}
+
+  if ((c == 27) && (decode.seqno == 0))
+     {decode.seqno = 1;
+      printf("unheralded esc character; start delayed processing %d\n",c);
+      decode.pr = 0;
+      return 1; }
+
+  int testa = (c==72)||(c==70);                   // home, end
+  int testb = (c==65)||(c==66)||(c==67)||(c==68); // arrow up, down, right, left
+  int testc = (c==50)||(c==51)||(c==53)||(c==54); // page up, page down, insert, delete
+
+  int testd = (testa || testb||testc);
+
+  int teste = !testd;
+
+  if (( c != 91 ) && (decode.seqno == 1)) 
+     {//printf("not a esc seq; processing enabled\n",c);
+      decode.seqno = 0;
+      decode.retchar = (int) c;
+      decode.pr = 1; 
+      return 2;}
+
+  if (( c == 91 ) && (decode.seqno == 1))
+     {//printf("second escape: delay processing %d\n",c);
+      decode.seqno=2;
+      decode.pr=0; 
+      return 3;}
+
+  if ((testa || testb) && (decode.seqno == 2)) 
+     {printf("ending short escape sequence %d\n",c);
+      decode.seqno = 0;
+      decode.pr=1;
+      decode.retchar= 1000;
+      return 4;}
+
+  if (testc && (decode.seqno == 2))
+     {//printf("still processing escape seq %d\n",c);
+      decode.seqno = 3;
+      decode.pr=0;
+      return 5;}
+
+  if ((c == 126) && (decode.seqno == 3))
+     {printf("ending long escape sequence %d\n",c);
+      decode.seqno = 0;
+      decode.retchar = 1000;
+      decode.pr=1;
+      return 6;}
+
+  printf("reaching here is unusual c = %d\n",c);
+  decode.pr=1;
+  decode.retchar = (int) c;
+  decode.seqno = 0; return 7;
 }
 
+struct termios orig_termios;
+
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
-    die("tcsetattr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw = E.orig_termios;
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-  raw.c_oflag &= ~(OPOST);
-  raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
 
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-char editorReadKey() {
-  int nread;
-  char c;
-  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-    if (nread == -1 && errno != EAGAIN) die("read");
-  }
-
-  if (c == '\x1b') {
-    char seq[3];
-
-    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
-    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-
-    if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
-      }
-    }
-
-    return '\x1b';
-  } else {
-    return c;
-  }
-}
-
-int getCursorPosition(int *rows, int *cols) {
-  char buf[32];
-  unsigned int i = 0;
-
-  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
-
-  while (i < sizeof(buf) - 1) {
-    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
-    if (buf[i] == 'R') break;
-    i++;
-  }
-  buf[i] = '\0';
-
-  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
-  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
-
-  return 0;
-}
-
-int getWindowSize(int *rows, int *cols) {
-  struct winsize ws;
-
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-    return getCursorPosition(rows, cols);
-  } else {
-    *cols = ws.ws_col;
-    *rows = ws.ws_row;
-    return 0;
-  }
-}
-
-/*** append buffer ***/
-
-struct abuf {
-  char *b;
-  int len;
-};
-
-#define ABUF_INIT {NULL, 0}
-
-void abAppend(struct abuf *ab, const char *s, int len) {
-  char *new = realloc(ab->b, ab->len + len);
-
-  if (new == NULL) return;
-  memcpy(&new[ab->len], s, len);
-  ab->b = new;
-  ab->len += len;
-}
-
-void abFree(struct abuf *ab) {
-  free(ab->b);
-}
-
-/*** output ***/
-
-void editorDrawRows(struct abuf *ab) {
-  int y;
-  for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-        "Kilo editor -- version %s", KILO_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
-        abAppend(ab, "~", 1);
-        padding--;
-      }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
-    } else {
-      abAppend(ab, "~", 1);
-    }
-
-    abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
-    }
-  }
-}
-
-void editorRefreshScreen() {
-  struct abuf ab = ABUF_INIT;
-
-  abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[H", 3);
-
-  editorDrawRows(&ab);
-
-  char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
-  abAppend(&ab, buf, strlen(buf));
-
-  abAppend(&ab, "\x1b[?25h", 6);
-
-  write(STDOUT_FILENO, ab.b, ab.len);
-  abFree(&ab);
-}
-
-/*** input ***/
-
-void editorMoveCursor(char key) {
-  switch (key) {
-    case ARROW_LEFT:
-      E.cx--;
-      break;
-    case ARROW_RIGHT:
-      E.cx++;
-      break;
-    case ARROW_UP:
-      E.cy--;
-      break;
-    case ARROW_DOWN:
-      E.cy++;
-      break;
-  }
-}
-
-void editorProcessKeypress() {
-  char c = editorReadKey();
-
-  switch (c) {
-    case CTRL_KEY('q'):
-      write(STDOUT_FILENO, "\x1b[2J", 4);
-      write(STDOUT_FILENO, "\x1b[H", 3);
-      exit(0);
-      break;
-
-    case ARROW_UP:
-    case ARROW_DOWN:
-    case ARROW_LEFT:
-    case ARROW_RIGHT:
-      editorMoveCursor(c);
-      break;
-  }
-}
-
-/*** init ***/
-
-void initEditor() {
-  E.cx = 0;
-  E.cy = 0;
-
-  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-}
-
-int main() {
+int main() 
+{
   enableRawMode();
-  initEditor();
 
-  while (1) {
-    editorRefreshScreen();
-    editorProcessKeypress();
-  }
+  int nread;
+  char c, seq[3];
+
+  while (1) 
+{   
+    nread = read(STDIN_FILENO,&c,1);  //character string from keypresses
+
+    if ( c == 'q' ) return 0;
+
+    int ignore = uncode(c);
+
+    if (!decode.pr) {continue;} // working on escape sequence
+
+    if (decode.retchar < 1000) 
+    
+    {c = (char) decode.retchar;
+      if (iscntrl(c)) {printf("%d\n", c);} 
+      else {printf("%d ('%c')\n", c, c);}
+    }
+    
+    else {printf("000 arrows, etcetera %d\n", decode.retchar); 
+          continue;}
+}
 
   return 0;
 }
-
