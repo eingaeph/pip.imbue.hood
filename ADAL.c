1fe9573e58a5a3a49edb9dc051532ac98d7d36f4
Howard Stewart <howarq@yahoo.com>
2017-09-30 13:05:41 +0000

    Add replacement functionality following initialization segment

--- ADAL.c
+++ ADAL.c
@@ -13,35 +13,58 @@ typedef struct slot
 
     slot line;
     slot *text;
+    slot *old;
+    slot *new;
 
     char *ptr;
 
 int main(void) {
 
+    text = malloc(10*sizeof(slot));
+    old  = malloc(10*sizeof(slot));
+    new  = malloc(10*sizeof(slot));
+   
     ptr = "this is my story";
-
     line.row = ptr;
     line.size = strlen(ptr);
-
-    text = malloc(10*sizeof(slot));
-    
     text[0] = line;
-    printf("text[0].row = %s\n",text[0].row);
+
 
     ptr = "this is my song";
     line.row = ptr;
     line.size = strlen(ptr);
-
     text[1] = line;
-    printf("text[1].row = %s\n",text[1].row);
-    printf("text[0].row = %s\n",text[0].row);
+  
  
     ptr = "tell me your song";
     line.row = ptr;
     line.size = strlen(ptr);
-
     text[3] = line;
+
     printf("text[3].row = %s\n",text[3].row);
+
+    old = text;
+    
+    int j;
+
+    for (j = 0; j < 10; j++) {old[j] = text[j];}
+    printf("old[1].row = %s\n",old[1].row);
+    printf("old[3].row = %s\n",old[3].row);
+
+    slot newline;
+    ptr = "hello world";
+    newline.row = ptr;
+    newline.size = strlen(ptr);
+
+    for (j = 0; j < 10; j++) 
+      {if (j != 3) {old[j] = text[j];}
+       else        {old[j] = newline;}
+      }
+    printf("\n");
+    printf("old[3].row = %s\n",old[3].row);
+    printf("old[1].row = %s\n",old[1].row);
+
+
 
     return 0;
 }
