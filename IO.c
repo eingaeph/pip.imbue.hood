2a091e04cbacb6af813949afc2630a86c57225d5
Howard Stewart <howarq@yahoo.com>
2017-12-05 23:10:35 +0000

    Initiate IO.c  replacement for getline

---
+++ IO.c
@@ -0,0 +1,70 @@
+#! /usr/bin/tcc -run
+
+/*** 
+   IO prototype  
+   Read a file line by line 
+***/
+
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+/*** terminal ***/
+
+struct termios orig_termios;
+
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** write (to the screen) ***/
+
+
+int main() {
+
+ enableRawMode();
+ int fd = open("test.dat",O_RDONLY);
+ char buf;
+ int ignore = read(fd,&buf,1);
+ write(1,&buf,sizeof(buf));
+ write(1,"\n\r",2);
+ close(fd);
+ exit(0);
+}
+
+
+
