3741cf6cdf2e358ec249d3923df4a74c9f8ab249
Howard Stewart <howarq@yahoo.com>
2018-01-11 19:11:07 +0000

    Correct sscanf call to pass addresses include (not used) function getCursorPosition

--- QERY.c
+++ QERY.c
@@ -11,6 +11,25 @@
 
 struct termios orig_termios;
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -66,25 +85,15 @@ int main() {
         i++;
     }
 
-    printf("i = %d\n",i);
     buf[i] = '\0';
 
-    int x; int y;
+    
     if (buf[0] != 27 || buf[1] != '[') return -1;
-
-    int leng = strlen(buf);
-    printf("string length = %d\n",leng);
-
-    printf("<esc>[24;80\n");
-    int j ;
-    for ( j = 2; j < 7; j++) 
-    printf("j = %d buf[%d] = %d\n", j,  j, buf + j);
                               
-//    sscanf(buf+2,"%d;%d",x,y);
+    int x; int y;
+    if (sscanf(buf+2,"%d;%d",&x,&y) != 2) return -1;
 
-//    if (sscanf(buf+2,"%d;%d",x,y) != 2) return -1;
-
-//    printf("x = %d , y = %d \n", x,y);
+    printf("x = %d , y = %d \n", x,y);
 
   }
 
