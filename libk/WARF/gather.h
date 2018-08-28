3173416784dbc50e84f42df6bb6dd933af0ce8aa
Howard Stewart <howarq@yahoo.com>
2018-08-28 21:52:58 +0000

    Add scrolling parameters comments to main.cold

--- libk/WARF/gather.h
+++ libk/WARF/gather.h
@@ -5,7 +5,7 @@
 //#include "/data.base/product/libk/UVSE/UVSE.h"
 //#include "/data.base/product/libk/UVSE/VT100.h"
 
-//#include "/data.base/product/libk/KEYP/KEYP.h"
+#include "/data.base/product/libk/KEYP/KEYP.h"
 
 #include "/data.base/product/libk/WARF/WARF.h"
 
