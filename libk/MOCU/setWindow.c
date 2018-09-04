6f5cc606b97941937b6fa39eea2571dc34e1ddf6
Howard Stewart <howarq@yahoo.com>
2018-09-04 23:11:06 +0000

    Update setWindow.c

--- libk/MOCU/setWindow.c
+++ libk/MOCU/setWindow.c
@@ -1,28 +1,31 @@
 #include "../libk.h"
 
-// given  text, ix,iy, numblines
+// given  text, ix,iy, numblines, xmin, ymin
 // given previous window xmin xmax ymin ymax
 //   set cursor position cu, cv
 //   return xmin, xmax, ymin, ymax 
 //
+// check for possibleWindow before returning
+//
 // possibleLine
+//    assert(       ((text[testy].row == NULL) &&       \
+//                   (text[testy].size == 0  ))         \
+//             ||                                       \
+//                  ((text[testy].row != NULL) &&       \
+//                   (text[testy].size >  0  ))         \
+//           );
 //
 // possibleIxIy
 //   0<=ix<=text[iy].size-1
 //   0<=iy<=numblines
 // 
-// test inWindow
-//   xmin<=ix<=xmax 
-//   ymin<=iy<=ymax
-// return immediately
-//
-// window u = 0,...,cols-1
-//        v = 0,.....rows-1
+// window u = 0,.....,cols-1
+//        v = 0,.....,rows-1
 // 
 // four corner possible test results
 // four edge non corner possible test results
 //
-// the corner cases do not require separate treatemnt
+// the corner cases do not require separate treatment
 // except that x and y adjustments must happen each time
 //   x test, then xmin, xmax set
 //   y test, then ymin, ymax set
@@ -34,47 +37,58 @@ void setWindow(void)
     int* xmin; int* xmax; int* ymin; int* ymax; 
     int* ix; int* iy;
     int* cu; int* cv;
+    int* numblines.
 
     xmin =&arg2.xmin; xmax = &arg2.xmax; ymin = &arg2.ymin; ymax= &arg2.ymax;
-    ix = &glob.ix; iy = &glob.iy;
+    ix = &glob.ix; iy = &glob.iy; numblines =  &glob.numblines
     cu = &arg2.cu; cv = &arg2.cv;
 
     possibleLine; possibleIxIy;
+
+    int testa, testb, testc, testd, teste;
     testa = (*ix >= *xmin);
     testb = (*ix <= *xmax);
     testc = (*iy >= *ymin);
     testd = (*iy <= *ymax);
     teste = testa && testb && testc && testd;
 
-    if (teste) 
-      {
-//Set Cursor Position	<ESC>[{ROW};{COLUMN}f
-    *cu = *ix + 1; *cv = *iy + 1
-    print("\x1b[%d;%df",*cv,*cu);
-    exit(0);
-       {
-    int ix = glob.ix; int iy = glob.iy; 
-    possibleIxIy; int testy = iy; possibleLine;
+    if (teste) {
+                *cu = *ix - *xmin; 
+                *cv = *iy - *ymin;
+                print("\x1b[%d;%df",*cv,*cu);
+                return;
+               }
 
-    if ((ix == jx) && (iy = jy)) return;
+//  adjust xmin, xmax
 
-    int ysize; if   (glob.numbLines < glob.vmax ) ysize = glob.numbLines;
-               else                               ysize = glob.vmax + 1;
+    asset(!testa || !testb || !testc || !testd);
 
-    int xsize = glob.umax - glob.umin + 1 ;
+    if(!testa) {*xmin = *ix; *xmax = *xmin + cols - 1;}
+    if(!testb) {*xmax = *ix; *xmin = *xmax - cols + 1;}
 
-    if (glob.ix > glob.xmax)  {glob.xmax = glob.ix; glob.xmin = glob.xmax - xsize;}
-    if (glob.ix < glob.xmin)  {glob.xmin = glob.ix; glob.xmax = glob.xmin + xsize;}
-    if (glob.iy > glob.ymax)  
-                  {
-                       glob.ymax = glob.iy;    
-                       glob.ymin = glob.ymax - ysize;
-                   if (glob.ymin < 0) glob.ymin = 0; 
-                  }
+//  adjust ymin, ymax
 
-    glob.cu = glob.ix - glob.xmin;
-    glob.cv = glob.iy - glob.ymin;
+    if (!testc) {*ymin = *iy; *ymax = *ymin + rows - 1;}
+    if (!testd) {*ymax = *iy; *ymin = *ymax - rows + 1;}
 
-    testy = glob.iy; possibleLine;
+// final test : possibleWindow
 
+//   xmin<=ix<=xmax 
+//   ymin<=iy<=ymax
+
+    testa = (*ix >= *xmin);
+    testb = (*ix <= *xmax);
+    testc = (*iy >= *ymin);
+    testd = (*iy <= *ymax);
+    teste = testa && testb && testc && testd;
+    assert(teste);
+
+    assert( xmax - xmin <= cols -1);
+    assett( ymax - ymin <= rows -1);
+
+    assert( xmin >= 0); assert(xmax > xmin);
+    assert( ymin >= 0);
+    assert(ymax <= numblines -1):
+
+    return;
 }
