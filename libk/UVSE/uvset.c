c7d991fdb43e0bc600c32e18cfd4995f9b2acf03
Howard Stewart <howarq@yahoo.com>
2018-10-20 22:32:29 +0000

    Make tiny mod to  uvset.c and retest

--- libk/UVSE/uvset.c
+++ libk/UVSE/uvset.c
@@ -1,17 +1,10 @@
 #include "../libk.h"
- 
-//  query screen for size
-//  clear display 
-//  display top of screen line (line 1 is top of screen)
-//  display tildes along left sign of screen
-//  display bottom of screen line (line rows is bottom of screen) 
-
+//  function uvset
 void uvset(void)
 {   
-
-    /* Place Cursor */ int lena = strlen( CursorToMaxForwardMaxDown );
-    write(STDOUT_FILENO, CursorToMaxForwardMaxDown, lena );
+//  query screen for size with getCursorPosition 
     int rows, cols; /* int retval = */ getCursorPosition(&rows,&cols);
+//  set struct glob variables rows, cols, umax, vmax
     glob.rows = rows; glob.cols = cols;
     glob.umax = cols -1 ; glob.vmax = rows -1 ;
 
