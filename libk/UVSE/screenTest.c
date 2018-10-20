892c39d588e9a16609b67177e912ac1564317838
Howard Stewart <howarq@yahoo.com>
2018-10-20 21:29:57 +0000

    Add comments suit for formatting with idiom

--- libk/UVSE/screenTest.c
+++ libk/UVSE/screenTest.c
@@ -1,23 +1,26 @@
 #include "../libk.h"
-
+//  function screenTest
 void screenTest(void)
 { 
+//  retrieve Screen rows and columns from struct glob
     int rows, cols; rows = glob.rows; cols = glob.cols;
-
+//  Move Screen Cursor to Top Left, Then Clear Screen
     printf("%s", CursorToTopLeft ClearScreen );fflush(stdout);
+//  Print the Screen Header
     printf("Top of Screen: rows = %d cols = %d\n\r", rows,cols);fflush(stdout);
 
     int count;
+//  Print Left Column Tildes to Screen, leaving screen last line clear
     for (count = 2; count < rows; count ++)
         {printf("%s", TildeReturnNewline); fflush(stdout);}
-
+//  Prnt Screen Last Line 
     printf("%s","Bottom of Screen         "); fflush(stdout);
 
-//  Force Cursor Position with	<ESC>[{ROW};{COLUMN}f
+//  Place Cursor Position with	<ESC>[{ROW};{COLUMN}f
 
     printf ("\x1b[%d;%df%s", 5,5,"Hello World"); fflush(stdout);
     
-//
+//  Move Screen Cursor to Second Line, First Column 
     printf("\x1b[%d;%df",2,1); fflush(stdout);
 
     return; 
