b8ebe2f5cb170bb7099339862de431406433622a
Howard Stewart <howarq@yahoo.com>
2018-10-30 21:03:31 +0000

    Begin the process of debuging REPL, which has never executed fully. At present draw.c is being fed bad data.

--- libk/REPL/draw.c
+++ libk/REPL/draw.c
@@ -9,7 +9,9 @@ void draw(void)
 
     printf(ClearScreen); fflush(stdout); 
     int cu = 0; int cv = 0;
-    printf("\x1b[%d;%df",cv,cu); fflush(stdout); 
+    printf("\x1b[%d;%df",cv,cu); fflush(stdout);
+    printf("xmin = %d xmax = %d\n\r", glob.xmin,glob.xmax); 
+    exit(0);
 
 //  draw the window beginning at the cursor
 
--- libk/REPL/init.c
+++ libk/REPL/init.c
@@ -4,7 +4,10 @@ void init(int argc, char** argv)
 {
     if(argc < 2) die("filename not entered as argument");
 
-    char * filename = argv[1]; readAfile(filename); 
+//  read input file, initialize text and numbLinesq
+
+    char * filename = argv[1]; 
+    glob.numbLines = readAfile(filename); 
 
 //  query screen for size, write initial window using rows and cols
 //  uvset returns values through standard input
@@ -12,13 +15,14 @@ void init(int argc, char** argv)
     enableRawMode(); uvset(); disableRawMode();
 
 //  write initial window using rows and cols, not xmin etc.
+//  printf("rows = %d\n\r",glob.rows); exit(0); 
 
-    printf("rows = %d\n\r",glob.rows); exit(0); 
     screenTest();
 
 //  set initial insertion point, in text coordinates of course
 
     glob.ix = 0; glob.iy = 0;
+  
     return;
 }
 
