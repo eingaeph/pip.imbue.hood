84d132283c93152c32400f561f755352756b9f63
Howard Stewart <howarq@yahoo.com>
2018-11-17 23:05:08 +0000

    Isolate a bug in setWindow.c and wind.c

--- libk/REPL/draw.c
+++ libk/REPL/draw.c
@@ -19,6 +19,13 @@ void draw(void)
  
 //  draw the window beginning at the cursor
   
+   printf("entering wind\n\r");
+   printf("xmin = %d xmax = %d\n\r",glob.xmin,glob.xmax);
+   printf("ymin = %d ymax = %d\n\r",glob.ymin,glob.ymax);
+   printf("ix   = %d iy   = %d\n\r",glob.ix,glob.iy);
+
+int iw = 7234567;  waiter(iw); // iw = 1234567;
+
     wind(glob.xmin,glob.xmax, glob.ymin, glob.ymax, glob.numbLines);
 
     return;
---
+++ libk/TESS/setWindow.4
@@ -0,0 +1,1 @@
+../EDAL/setWindow.c
