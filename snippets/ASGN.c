418db02227736575c1a3d9b7449c397169e7360f
Howard Stewart <howarq@yahoo.com>
2018-11-22 23:18:11 +0000

    Document final ASGN idioms for initializing slots and arrays of slots.

--- snippets/ASGN.c
+++ snippets/ASGN.c
@@ -7,41 +7,38 @@
 #include <string.h>
 #include <assert.h>
 
-typedef struct {ssize_t size; char *row; int count;} slot;
-
-    slot line; slot *text;
+typedef struct {ssize_t size; char *row; int count;} slot; slot line; slot *text;
 
 int main(void)
 {
+  slot aline, bline, cline;
 
-  slot aline;
   aline.row = "sing that story";      //memory for string allocated here
   aline.size = strlen(aline.row);
 
-  slot bline;
-
-  slot cline;
   cline.row = "sorry charlie";        //memory for string allocated here
   cline.size = strlen(cline.row);
 
-// use memcpy instead of assignment operator =
+// use memcpy instead of assignment operator = to iniitialize a slot
 // void *memcpy(void *dest, const void *src, size_t n);
 
-  memcpy(&bline,&aline,sizeof(slot)); 
+   memcpy(&bline,&aline,sizeof(slot)); 
 
-  assert(aline.row == bline.row);
+// assert(aline == bline) fails to compile 
+   assert(aline.row == bline.row); // succeeds during execution
 
 // The malloc() function allocates size bytes and returns a pointer to the
 // allocated memory. 
 
-// Whenever an expression such as X[k] 
-// appears in a program, 
-// the compiler interprets it to mean *(X + k)
-
   slot *book = malloc(5*sizeof(slot)); //book = pointer to contiguous memory for 5 slots
                                        //book + 0 = pointer to first slot
                                        //book + 1 = pointer to second slot 
- 
+
+  assert( (book+2) == &book[2] );
+
+// Whenever an expression such as X[k] appears in a program, 
+// the compiler interprets it to mean *(X + k)
+
   memcpy(book+0, &aline, sizeof(slot));
   memcpy(book+1, &bline, sizeof(slot));
   memcpy(book+2, &cline, sizeof(slot));
@@ -51,19 +48,16 @@ int main(void)
   printf("\n");
 
   slot *tabl = malloc(5*sizeof(slot));
+  assert( (tabl+2) == &tabl[2] );
   memcpy(tabl+2,book+2,sizeof(slot));
   printf("%s\n",tabl[2].row);
   printf("\n");
 
-  assert( (book+2) == &book[2] );
-  assert( (tabl+2) == &tabl[2] );
+  memcpy(&tabl[1],&book[1],sizeof(slot));
+  assert( tabl+1 == &tabl[1]);
+  printf("%s\n",tabl[1].row);
+  printf("%s\n",(tabl+1)->row);
 
-  memcpy(&tabl[2],&book[2],sizeof(slot));
-  printf("almost\n\r");
-  printf("%s\n",tabl[2].row);
-  printf("\n");
-  
-  
   printf("stopping\n");
   return 0;
 }
