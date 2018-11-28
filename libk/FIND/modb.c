2c8597b84aeff6a4312fd8400a4c43b10e66b4d8
Howard Stewart <howarq@yahoo.com>
2018-11-28 17:11:22 +0000

    Make mods sufficent to run an attempted search

--- libk/FIND/modb.c
+++ libk/FIND/modb.c
@@ -3,16 +3,16 @@
 void modb(int retval)
 {
    char query[100]; char *s = &query[0];
+printf("here i am\n\r"); 
    while(1)
    {
     int retval = ReadKey(); if (retval == CTRL_S) break;
     char c = retval;         // retrieve a 1 byte character 
                              // from a 4 byte intege
     *s = c; s++;
-   }
-
     *s = '\0';
-    printf("query = %s",query); exit(0);
+    printf("query = %s\n\r",query); 
+   }
 
 // loop through text searching for query
   int i;
@@ -20,10 +20,12 @@ void modb(int retval)
    {int j = i + glob.iy; 
     if (j >= glob.numbLines) j = j - glob.numbLines; 
     char *match = strstr(text[j].row, query);
-    if (match) {glob.cv = j; 
-                glob.cu = (int) (match - text[j].row);
+    if (match) 
+              {
+                glob.iy = j; 
+                glob.ix = (int) (match - text[j].row);
                 break;
-               }
+              }
     }
 
 }
