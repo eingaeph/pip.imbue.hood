23ceec4072197c501051efc270cc871515925872
Howard Stewart <howarq@yahoo.com>
2018-11-30 18:06:04 +0000

    Add updates to find function. Testing still shows bugs.

--- libk/FIND/modb.c
+++ libk/FIND/modb.c
@@ -14,22 +14,68 @@ void modb(int retval)
     printf("query = %s\n\r",query); 
    }
 
+// check for a valid query
+
+   assert(strlen(query) >= 0);
+   if (strlen(query) == 0) 
+        {
+          possibleLine; possibleIxIy;
+          return;
+        }
+
+// check that the current line is long enough to contain query
+
+   if(text[glob.iy].size - strlen(query) <= 0)
+         {
+           possibleLine; possibleIxIy;
+           return;
+         }
+
 // check the current line for the search string
 
-   char *s = text[glob.iy].row + glob.ix;
-   int size = text[glob.iy].size - glob.ix;
-   char *match = memmem(s,size,query,strlen(query)); 
+   char* match = memmem(text[glob.iy].row,
+                        text[glob.iy].size,
+                        query,
+                        strlen(query));
 
-   int testa = (int) (match - text[glob.iy].row);
-   int testb = (match != NULL);
-   int testc = (testa != glob.ix);        
-   if (testb && testc) // match occurs but not at the insertion point
-        {
-         glob.ix = (int) (match -text[glob.iy].row);
-         possibleLine; possibleIxIy;
-         return;
-        } 
+// starting search in a line containing query
+int iw = 4234567; printf("at a\n\r"); waiter(iw);
 
+   if (match != NULL) 
+      { 
+printf("at b\n\r"); waiter(iw);
+        int testa = ( (int) (match - text[glob.iy].row) == 0); //check for immediate find
+printf("testa = %d\n\r",testa); waiter(iw);
+        if (!testa) 
+              {
+               glob.ix = (int) (match - text[glob.iy].row);
+               possibleLine; possibleIxIy;
+               return;
+              }
+printf("at c\n\r");  waiter(iw);
+// tests for immediate find
+
+        int lenrest = text[glob.iy].size - 2*strlen(query) -glob.ix;
+        int testb = (lenrest <=0 );
+        char* rest  = text[glob.iy].row + glob.ix + strlen(query);
+        char* natch = NULL;
+        if (!testb) natch = memmem(rest,
+                                   lenrest,
+                                   query,
+                                   strlen(query));
+        int testc = (natch == NULL); 
+
+// skip immediate find in first line check 
+
+         if (!testb && !testc )
+             {
+               assert( (int) (natch - rest) > 0);
+               glob.ix = glob.ix + strlen(query) + (int) (natch - rest); 
+               possibleLine; possibleIxIy;
+               return;  
+              } 
+   
+       }
 // loop through text searching for query, not to be found in the first line i=0
 
   int i;
