e24242bef3ec7a7e2350bcab212317d37d4f4b9a
Howard Stewart <howarq@yahoo.com>
2019-02-25 19:04:15 +0000

    Correcta bug or two in bildHL.c. A bug remains in bildHL when the final bracket is the end of file. Yuck. Tomorrow is another day.

--- libk/REND/bildHL.c
+++ libk/REND/bildHL.c
@@ -45,13 +45,13 @@ void bildHL(int option, int look, char* abuff)
         int ndex;  /* ndex is the index into abuff (and s) */
         for (ndex=0; s[ndex]; ndex++) 
           {
-           if ( (s[ndex]== '{') && (k != 0)) 
+           if ( (s[ndex]== '{') && (k == 0)) 
              {arg3[0].HLindex = ndex;    arg3[0].HLchange = red; }
            if (s[ndex]=='{')  {i++; k++;}
            if (s[ndex]=='}')  i--;
            if ((i == 0) && (k > 0)) break;
           };    
-        arg3[1].HLindex = ndex;    arg3[1].HLchange = revert; 
+        arg3[1].HLindex = ndex+1;    arg3[1].HLchange = revert; 
 //      if (i == 0) printf("the bracket was closed with k = %d \n",k);
         return;
    }
