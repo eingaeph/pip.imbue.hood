cd1592d5cf541f912306561352c650bbe48c776f
Howard Stewart <howarq@yahoo.com>
2019-03-12 21:16:03 +0000

    Bring MODA and FIND into agreement, leaving wind.c as the screen write routine.

--- libk/MODA/moda.c
+++ libk/MODA/moda.c
@@ -4,13 +4,6 @@
 
 void moda(int retval)
 {
-
-    setWindow();
-
-    headwind();
-    wind(glob.xmin,glob.xmax, glob.ymin, glob.ymax, glob.numbLines);
-    tailwind();
-
     editAline(retval, glob.iy); 
 
     return;
